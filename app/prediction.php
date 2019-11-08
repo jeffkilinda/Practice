c7d3e42c4b743668b3b78784feafc99f7a2645f3
jeffkilinda <jeffkilindah@gmail.com>
2019-11-08 13:26:50 +0300

    After changing table to be included across all files11

--- app/prediction.php
+++ app/prediction.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class prediction extends Model
+class Prediction extends Model
 {
    public function tips(){
 return $this->belongsToMany('App\Tip');
